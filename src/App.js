3b012c96a971b1f91b91af9948011654daf4bb3d
Linus Ljung <linus.ljung@outlook.com>
2018-05-03 21:43:50 +0200

    Added loader

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import Loader from './components/Loader';
 
 class App extends Component {
   state = {
-    isLoggedIn: true
+    isLoggedIn: true,
+    isLoading: false
   };
 
   handleButtonClick = (e) => {
     e.preventDefault();
 
     this.setState({
-      isLoggedIn: false
+      isLoading: true
     });
+
+    window.setTimeout(() => {
+      this.setState({
+        isLoggedIn: false,
+        isLoading: false
+      });
+    }, 1500);
   };
 
   render() {
     return (
       <div className='app'>
         <div className='content'>
-          { this.state.isLoggedIn ? (
+          { this.state.isLoading ? (
+            <Loader />
+          ) : this.state.isLoggedIn ? (
             <div>
-              <div className='text'>Du är inloggad!</div>
+              <div className='text'>{ 'Du är inloggad!' }</div>
               <div
                 className='button'
                 onClick={ this.handleButtonClick }
-              >Logga ut</div>
+              >{ 'Logga ut' }</div>
             </div>
           ) : (
             <div>
-              <div className='text'>Du har loggat ut.</div>
+              <div className='text'>{ 'Du har loggat ut.' }</div>
             </div>
           ) }
         </div>
---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './css.css';
+
+class Loader extends Component {
+  render() {
+    return <div class="spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
+  }
+}
+
+export default Loader;
